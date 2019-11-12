81403a41db2f3449d39e2fe1b183bf538330c875
wangxiwen <2213100406@qq.com>
2019-11-12 19:19:39 +0800

    修改配置

--- site/src/main/java/com/aba/pindan/site/DemoController.java
+++ site/src/main/java/com/aba/pindan/site/DemoController.java
@@ -1,13 +1,17 @@
 package com.aba.pindan.site;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Value("${elasticseach.host}")
+    private String urlPath;
+
     @RequestMapping("/test")
     public String test() {
-        return "this is test";
+        return "this is test--"+urlPath;
     }
 }
