70538855c89b50b2396d14e3e59c0c912b2821c1
wangxiwen <2213100406@qq.com>
2019-11-13 16:11:53 +0800

    添加mapper注解

--- dao/src/main/java/com/aba/pindan/dao/mapper/TestMapper.java
+++ dao/src/main/java/com/aba/pindan/dao/mapper/TestMapper.java
@@ -1,7 +1,9 @@
 package com.aba.pindan.dao.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+@Mapper
 public interface TestMapper {
 
     long insert(@Param("name") String name) throws Exception;
